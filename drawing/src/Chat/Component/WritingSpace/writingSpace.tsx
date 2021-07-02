0202375b72675cbdb3b9ee4c1b771180b0dd675c
Anraku Honoka <rakuraku2563nonnon@gmail.com>
2021-07-02 22:44:42 +0900

    settings2

---
+++ drawing/src/Chat/Component/WritingSpace/writingSpace.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyle = makeStyles(() =>
+  createStyles({
+    input: {
+      gridRow: 2,
+      gridColumn: 2
+    },
+    buttonBox: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      gridRow: 2,
+      gridColumn: 3
+    },
+    button: {
+      width: '50%',
+      height: '75%',
+      display: 'block',
+      backgroundColor: '#ffea00',
+      '&:hover': {
+        backgroundColor: '#008394'
+      }
+    }
+  })
+);
+
+type WritingSpaceProps = {
+  handleContentChange: (
+    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => void;
+  submitError: boolean;
+  message: string;
+};
+
+const WritingSpace: React.FC<WritingSpaceProps> = (props: WritingSpaceProps) => {
+  const styles = useStyle();
+  const {
+    handleContentChange = () => undefined,
+    submitError = false,
+    message = ''
+  } = props;
+  return (
+    <>
+      {submitError ? (
+        <TextField
+          className={styles.input}
+          margin='dense'
+          id='message'
+          label='内容'
+          fullWidth
+          autoFocus
+          multiline
+          rows={3}
+          value={message}
+          error
+          helperText={'メッセージが入力されていません'}
+        />
+      ) : (
+        <TextField
+          className={styles.input}
+          margin='dense'
+          id='message'
+          label='内容'
+          fullWidth
+          autoFocus
+          multiline
+          rows={3}
+          value={message}
+        />
+      )}
+      <Box className={styles.buttonBox}>
+        <Button className={styles.button}>{'送信'}</Button>
+      </Box>
+    </>
+  );
+};
+
+export default WritingSpace;
--- drawing/src/Chat/Component/chat.tsx
+++ drawing/src/Chat/Component/chat.tsx
@@ -1,22 +1,22 @@
-import React from "react";
-import { makeStyles, createStyles } from "@material-ui/core/styles";
-import Box from "@material-ui/core/Box";
-import Chat from "../Container/chat";
+import React from 'react';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import WritingSpace from '../Container/WritingSpace/writingSpace';
 
 const useStyle = makeStyles(() =>
   createStyles({
-    display:{
-        gridRow:4,
-        gridColumn:2
+    display: {
+      gridRow: 4,
+      gridColumn: 2
     },
-    writingSpace:{
-        display:'grid',
-        gridTemplateRows:'2% 96% 2%',
-        gridTemplateColumns:'2% 68% 28% 2%',
-        width:'100%',
-        gridRow:6,
-        gridColumn:2,
-        borderColor:'#00695f'
+    writingSpace: {
+      display: 'grid',
+      gridTemplateRows: '2% 96% 2%',
+      gridTemplateColumns: '2% 68% 28% 2%',
+      width: '100%',
+      gridRow: 6,
+      gridColumn: 2,
+      borderColor: '#00695f'
     }
   })
 );
@@ -24,10 +24,12 @@ const useStyle = makeStyles(() =>
 const Chat: React.FC = () => {
   const styles = useStyle();
   return (
-    <Box className={styles.display}>
-      <Chat />
-    </Box>
-    <Box className={styles.writingSpace}
+    <>
+      <Box className={styles.display}></Box>
+      <Box className={styles.writingSpace}>
+        <WritingSpace />
+      </Box>
+    </>
   );
 };
 
